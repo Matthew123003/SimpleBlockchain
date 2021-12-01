e96b2fbafb68fe655130191169d480f530c5f8f0
Kristofer <kris@youngers.org>
2021-12-01 10:31:15 -0500

    starting on README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SimpleBlockchain
 a very simple in memory blockchain
 
+## What's a blockchain?
+
+A _blockchain_ is a straight-forward idea: it is a data structure that is resistant modification and corruption.
+It's used in **bitcoin** as mean to handle the tracking of a series of transactions in a way that is transparent to all the people sharing the data structure.
+
+Data Structures are central to almost all computing - things like Queues and Arrays and even Databases are used throughout a system to keep its data ordered and tracked in a reliable manner. It would be pretty terrible if every time a bank's main financial system restarted, you had to type in all the customer's data.
+
--- src/main/java/Chain.java
+++ src/main/java/Chain.java
@@ -1,5 +1,33 @@
 public class Chain {
     public static void main(String[] args) {
+        Chain.chain1();
+        Chain.chain2();
+    }
+
+    private static void chain1() {
+        // show a normal blockchain
+        System.out.println("Chain");
+
+        Blockchain<Block> chain = new Blockchain<Block>();
+        Block tb = null;
+
+        tb = chain.addBlock("{Deposit: $100.00 to: Jim's Account}");
+
+        tb = chain.addBlock("{Transfer: $50.00 from: Jim's Account to: Joan's Account}");
+
+        tb = chain.addBlock("{Withdraw: $10.00 from: Joan's Account}");
+
+        tb = chain.addBlock("{Transfer: $20.00 from: Joan's Account to: Kim's Account}");
+
+        // Output the state of the chain.
+        System.out.println("Final Blockchain");
+        System.out.println(chain.toString());
+        System.out.printf("Resultant Chain: %s\n\n", chain.isValid()? "IS VALID" : "ERROR is not VALID");
+
+    }
+
+    public static void chain2() {
+        // create a chain, and then corrupt it
         System.out.println("Chain");
 
         Blockchain<Block> chain = new Blockchain<Block>();
